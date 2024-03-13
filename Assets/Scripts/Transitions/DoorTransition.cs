fab383d19f3b60bf61f397360e20128caada13dc
Mikey <mikeybess27@gmail.com>
2024-03-13 15:09:03 -0500

    Level Edits, Fixing Code and Adding Comments

--- Assets/Scripts/Transitions/DoorTransition.cs
+++ Assets/Scripts/Transitions/DoorTransition.cs
@@ -4,13 +4,26 @@ using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class DoorTransition : MonoBehaviour, Interactable
 {
-    [SerializeField] private Light2D _bigLight;
-    [SerializeField] private List<Light2D> _lights;
+    [Header("Respawn")]
     [SerializeField] private Vector2 _newRespawnPoint;
+    
+    [Header("Particle Prefab")]
     [SerializeField] private GameObject _particles;
+    
+    [Header("Big Light")]
+    [SerializeField] private Light2D _bigLight;
+    [SerializeField] private float _bLightIntensity;
+    [SerializeField] private float _bLightFadeTime;
+    
+    [Header("Other Lights (Children)")]
+    [SerializeField] private bool _hasRandomDelay;
+    [SerializeField] private float _oLightIntensity;
+    [SerializeField] private float _oLightFadeTime;
     
     private RoomManager _rm;
     private SpriteRenderer _sr;
@@ -22,26 +35,54 @@ public class DoorTransition : MonoBehaviour, Interactable
         _sr = GetComponent<SpriteRenderer>();
         _bc = GetComponent<BoxCollider2D>();
     }
-    
+
     public void Interact()
     {
         // Hide Sprite (Make an animation later)
         _sr.color = Color.clear;
         _bc.enabled = false;
-        
+
         // Instantiate Particles
         Instantiate(_particles, transform.position, quaternion.identity);
-        
-        // If there's a _bigLight, activate it
-        _bigLight?.gameObject.SetActive(true);
-        
-        // Activate or fade in smaller lights (Coroutine later)
-        foreach (Light2D light in _lights)
+
+        // If there's a _bigLight, fade it in
+        if (_bigLight != null)
         {
-            light.intensity = 1;
+            StartCoroutine(FadeLight(_bigLight, _bLightIntensity, _bLightFadeTime));
         }
-        
+
+        // Fade in other lights, must be children of the door. 
+        foreach (Transform child in transform)
+        {
+            // Ref
+            Light2D childLight = child.GetComponent<Light2D>();
+            // Makes sure it's enabled
+            childLight.enabled = true;
+            
+            // Coroutine with an if statement. Checks if small lights should fade in at random times or together
+            StartCoroutine(_hasRandomDelay
+                ? FadeLight(childLight, _oLightIntensity, Random.Range(0f, _oLightFadeTime))
+                : FadeLight(childLight, _oLightIntensity, _oLightFadeTime));
+        }
+
         // Set new checkpoint respawn point
         _rm.SetRespawnPoint(_newRespawnPoint);
     }
+
+    private IEnumerator FadeLight(Light2D lightToFade, float intensity, float fadeTime)
+    {
+        float elapsedTime = 0f;
+        float initialIntensity = lightToFade.intensity;
+        while (elapsedTime < fadeTime)
+        {
+            float newIntensity = Mathf.Lerp(initialIntensity, intensity, elapsedTime / fadeTime);
+            lightToFade.intensity = newIntensity;
+            elapsedTime += Time.deltaTime;
+            yield return null; // Wait for the next frame
+        }
+
+        // Ensure the intensity is set to the target value at the end
+        lightToFade.intensity = intensity;
+    }
+    
 }
